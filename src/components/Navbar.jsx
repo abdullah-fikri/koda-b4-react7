fe3de13a54c53cf72ce0d43d997fde4c96fa6fa0
Abdulah-fikri <abdullahfikri981@gmail.com>
2025-10-07 13:55:21 +0700

    added Navbar components

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="flex items-center justify-between px-8 py-6 border-b border-gray-100">
+      <h1 className="text-indigo-400 font-semibold text-3xl">To Do List</h1>
+    </div>
+  );
+}
